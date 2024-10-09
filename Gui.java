ea9d73273c174491cd7cd3ce992fbdf395d4214e
Noy Sheyman <noysheyman@icloud.com>
2024-10-09 00:22:27 -0700

    keep working on learning Gui

--- Gui.java
+++ Gui.java
@@ -1,6 +1,12 @@
 //using https://www.youtube.com/watch?v=Kmgo00avvEw&t=239s as tutorial 
 
+import java.io.File; 
+import java.io.IOException;
+import java.io.FileWriter;
+import java.lang.Thread;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 
@@ -13,6 +19,7 @@ public class Gui {
     ImageIcon logo;
     Color background;
     Border border;
+    
 
     
     
@@ -25,21 +32,21 @@ public class Gui {
      
 
         
-
+        frame.setLayout(null);
         frame.setTitle("15-PUZZLE");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setResizable(true);
         frame.setIconImage(logo.getImage());
         frame.getContentPane().setBackground(background);
-        //frame.setLayout(null);
+        frame.setMinimumSize(new Dimension(900,900));
+        
         
         logo.setImage(logo.getImage().getScaledInstance(500,500, Image.SCALE_SMOOTH));
         
 
-        //label.setBounds(100,100,600,600);
+        label.setBounds(150,150,600,600);
         label.setForeground(Color.white);
-        //label.setBackground(Color.green);
         label.setBorder(border);
         //label.setOpaque(true);
         label.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
@@ -49,12 +56,34 @@ public class Gui {
         label.setHorizontalTextPosition(JLabel.CENTER); //of image icon
         label.setVerticalTextPosition(JLabel.TOP);
         label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.TOP);
+        label.setVerticalAlignment(JLabel.CENTER);
+
 
         
         
+
         frame.add(label);
         frame.setVisible(true);
         frame.pack();
+
+
+
+        /*try {
+            File temp = new File("temp.txt");
+            temp.createNewFile();
+            FileWriter typer = new FileWriter("temp.txt");
+            while(true) {
+                try {
+                    Thread.sleep(200); 
+                } catch (Exception e) {
+                    System.out.println("hi");
+                }
+                typer.write(frame.getSize() + "\n"); }
+
+        } catch (IOException e) {
+            System.out.println("asdfasd");
+        } */
+
+        
     }
 }
